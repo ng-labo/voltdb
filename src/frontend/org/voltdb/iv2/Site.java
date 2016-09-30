37094e24e5f2273dbb308f53a77d92270f118bb9
wweiss <wweiss@voltdb.com>
2016-09-30 15:36:04 -0400

    ENG-10539: Avoid copying Parameters for CRC generation of write statements

--- src/frontend/org/voltdb/iv2/Site.java
+++ src/frontend/org/voltdb/iv2/Site.java
@@ -29,6 +29,7 @@ import java.util.ListIterator;
 import java.util.Map;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.zip.CRC32;
 
 import org.voltcore.logging.Level;
 import org.voltcore.logging.VoltLogger;
@@ -1427,6 +1428,8 @@ public class Site implements Runnable, SiteProcedureConnection, SiteSnapshotConn
             long[] planFragmentIds,
             long[] inputDepIds,
             Object[] parameterSets,
+            boolean[] isWriteFrag,
+            CRC32 writeCRC,
             String[] sqlTexts,
             long txnId,
             long spHandle,
@@ -1438,6 +1441,8 @@ public class Site implements Runnable, SiteProcedureConnection, SiteSnapshotConn
                 planFragmentIds,
                 inputDepIds,
                 parameterSets,
+                isWriteFrag,
+                writeCRC,
                 sqlTexts,
                 txnId,
                 spHandle,
